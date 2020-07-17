15cb95ff1af1b86b4439b9a2fdc07e93ec19c721
Darlan Alves <me@darlanalv.es>
2020-07-17 14:11:40 +0200

    feat: add markdown upgrade to index page

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,22 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
+import * as FS from 'fs';
+import * as Path from 'path';
 import apiRoutes from './routes';
 import { LOG } from './common';
+
+const readme = FS.readFileSync(Path.join(process.cwd(), 'README.md')).toString('utf8');
+const index = FS.readFileSync(Path.join(process.cwd(), 'index.html'))
+  .toString('utf8')
+  .replace('%content%', readme);
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json({ strict: false }));
 app.use(cors());
-app.get('/', (_, res) => res.sendFile(require('path').join(process.cwd() , 'README.md')));
+app.get('/', (_, res) => res.send(index));
 app.get('/favicon.ico', (_, res) => res.status(404).send(null));
 app.use(apiRoutes);
 app.use((_, res) => res.status(404).send('Not found'));
